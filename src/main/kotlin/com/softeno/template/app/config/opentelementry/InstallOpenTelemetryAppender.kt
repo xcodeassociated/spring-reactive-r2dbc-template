876b202595134f94495aebc678e90deac3fc5e29
xcodeassociated <janusz.majchrzak@protonmail.com>
2026-02-27 19:46:19 +0100

    add spring opentelemetry

---
+++ src/main/kotlin/com/softeno/template/app/config/opentelementry/InstallOpenTelemetryAppender.kt
@@ -0,0 +1,17 @@
+package com.softeno.template.app.config.opentelementry
+
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.stereotype.Component
+
+
+@Component
+class InstallOpenTelemetryAppender(
+    private val openTelemetry: OpenTelemetry
+) : InitializingBean {
+
+    override fun afterPropertiesSet() {
+        OpenTelemetryAppender.install(openTelemetry)
+    }
+}
